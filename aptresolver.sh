2dc19e344c7456ce2c172ede6a803194afde8f95
Vincenzo Talarico <56183806+oneafter909@users.noreply.github.com>
2020-12-03 18:07:55 +0100

    Create aptresolver.sh

---
+++ aptresolver.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+clear
+echo "[APTResolver]"
+if [ `id -u` != 0 ]; then
+   echo "E: Permesso Negato" 
+   exit 1
+fi
+fileLista="/etc/apt/source.list"
+echo "Inserisci nome della repository da aggiungere"
+read NOMEREPO
+echo "deb http://ppa.launchpad.net/$NOMEREPO/ppa/ubuntu buster main" >> $fileLista #buser è il codename dell'ultima versione di Debian
+#Inserisce riga alla source list
+echo "deb-src http://ppa.launchpad.net/$NOMEREPO/ppa/ubuntu buster main " >> $fileLista #Inserisce riga alla source list
+echo "Inserisci il fingerprint della repository"
+read CHIAVE
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv $CHIAVE
+sudo apt update
+sudo dpkg --configure -a
+sudo apt --fix-broken install
